9418e768715b21897481632d5b59fe484264de42
IceMimosa <chk19940609@gmail.com>
2021-06-07 14:01:12 +0800

    Add docs example

---
+++ .docgenirc.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('@docgeni/core').DocgeniConfig}
+ */
+module.exports = {
+    mode: 'full',
+    title: 'Bitlap',
+    repoUrl: 'https://github.com/bitlap/bitlap',
+    navs: [
+        null,
+        {
+            title: 'GitHub',
+            path: 'https://github.com/bitlap/bitlap',
+            isExternal: true
+        }
+    ]
+}
+
