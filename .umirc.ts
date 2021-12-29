5dff0f7635c505b85f22bbadf58928e94002b9b1
IceMimosa <chk19940609@gmail.com>
2021-12-30 02:32:58 +0800

    Add logo

--- .umirc.ts
+++ .umirc.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   title: repo,
   favicon:
     // "https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png",
-    "http://ice-img.flutterdart.cn/2021-12-29-ic_fish.png",
+    "/logo.png",
   logo:
     // "https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png",
-    "http://ice-img.flutterdart.cn/2021-12-29-ic_fish.png",
+    "/logo.png",
   outputPath: "docs-dist",
   mode: "site",
   hash: true,
